f4b02af7bd11b8d35413f715b53e2b32e31246ff
Oskar van Velden <oskar@rakso.nl>
2019-02-19 08:17:22 +0100

    Add game to git

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField]
+    Text textComponent;
+
+    [SerializeField]
+    State startingState;
+
+    State currentState;
+
+    public static bool isPrince = false;
+    public static bool karelPickedUp = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        currentState = startingState;
+
+        UpdateTextComponent();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ManageState();
+    }
+
+    private void ManageState() {
+        var nextStates = currentState.GetNextStates();
+
+        for (int index = 0; index < nextStates.Length; index++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index)) {
+                if (currentState == startingState) {
+                    isPrince = index == 0;
+                }
+
+                if (currentState.IsKarelChoice()) {
+                    karelPickedUp = index == 0;
+                }
+                
+                currentState = nextStates[index];
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            ResetGame();
+        }
+
+        UpdateTextComponent();
+    }
+
+    private void UpdateTextComponent()
+    {
+        textComponent.text = currentState.GetStateStory();
+    }
+
+    private void ResetGame() {
+        currentState = startingState;
+        isPrince = false;
+        karelPickedUp = false;
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)]
+    [SerializeField] 
+    string storyText;
+
+    [SerializeField]
+    State[] nextStates;
+
+    [SerializeField]
+    bool karelChoice = false;
+
+    public string GetStateStory() {
+        var returnValue = storyText;
+
+        returnValue = ReplaceGenderBasedTexts(returnValue);
+        returnValue = ReplaceKarelBasedTexts(returnValue);
+
+        return returnValue;
+    }
+
+    private string ReplaceGenderBasedTexts(string text) {
+        string returnValue = text;
+
+        Regex regex = new Regex(@"(\[[a-z]*/[a-z]*\])"); 
+        MatchCollection matches = regex.Matches(returnValue);
+
+        foreach (Match match in matches) {
+            GroupCollection groups = match.Groups;
+            
+            string combinedChoices = groups[1].ToString();
+
+            string[] choices = combinedChoices.Replace("[", "").Replace("]", "").Split('/');
+            string pickedChoice = AdventureGame.isPrince ? choices[0] : choices[1];
+
+            returnValue = returnValue.Replace(combinedChoices, pickedChoice);
+        }
+
+        return returnValue;
+    }
+
+    private string ReplaceKarelBasedTexts(string text) {
+        string returnValue = text;
+
+        Regex regex = new Regex(@"(\[karel:\s(?:.|\n|\r)*\])"); 
+        MatchCollection matches = regex.Matches(returnValue);
+
+        foreach (Match match in matches) {
+            GroupCollection groups = match.Groups;
+            
+            string foundMatch = groups[1].ToString();
+
+            if (!AdventureGame.karelPickedUp) {
+                returnValue = returnValue.Replace(foundMatch, "");
+            } else {
+                returnValue = returnValue.Replace(foundMatch, foundMatch.Replace("[karel: ", "").Replace("]", ""));
+            }
+        }
+        return returnValue;
+    }
+
+    public bool IsKarelChoice() {
+        return karelChoice;
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+}
